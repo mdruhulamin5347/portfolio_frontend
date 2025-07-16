28c369a6cc1f616bd7d4423ea832020b4a6c66a8
mdruhulamin5347 <mdruhulamin534793@gmail.com>
2025-07-16 22:53:36 +0600

    fix: delete server js

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -190,7 +190,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("bef77f47767ffacf")
+/******/ 		__webpack_require__.h = () => ("7852cb0060f4af98")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
